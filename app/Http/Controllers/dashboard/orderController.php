ceec136da6506d7baeea48c9a6736ec8f9b334e5
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-08-24 00:31:30 +0100

    Fixing case sensitive for models

--- app/Http/Controllers/dashboard/orderController.php
+++ app/Http/Controllers/dashboard/orderController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Order;
+use App\Models\order;
 use App\Models\orders_has_product;
 
 class orderController extends Controller
