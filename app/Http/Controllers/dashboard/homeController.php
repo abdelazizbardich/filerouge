b1d6faecf146d8523fd067786d3dcda5b3919ed4
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-08-24 00:20:53 +0100

    Fixing case sensitive for models

--- app/Http/Controllers/dashboard/homeController.php
+++ app/Http/Controllers/dashboard/homeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Product;
+use App\Models\product;
 use App\Models\Order;
 
 class homeController extends Controller
