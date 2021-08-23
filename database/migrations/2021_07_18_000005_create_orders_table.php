dc59f469a686983b3d3a225b4bf92e48f8c94230
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-08-23 22:48:29 +0100

    Invalid default value for 'state' on orders mogration fixed

--- database/migrations/2021_07_18_000005_create_orders_table.php
+++ database/migrations/2021_07_18_000005_create_orders_table.php
@@ -22,7 +22,7 @@ class CreateOrdersTable extends Migration
             $table->unsignedBigInteger('users_id');
             $table->unsignedBigInteger('shipping_adresses_id');
             $table->string('code', 100)->nullable()->default('000000');
-            $table->integer('state',1)->unsigned()->nullable()->comment("0: panding, 1: approved, 2: finised");
+            $table->integer('state',1)->nullable()->comment("0: panding, 1: approved, 2: finised");
             $table->decimal('total_price', 11, 2)->nullable()->default(0.00);
             $table->timestamps();
 
